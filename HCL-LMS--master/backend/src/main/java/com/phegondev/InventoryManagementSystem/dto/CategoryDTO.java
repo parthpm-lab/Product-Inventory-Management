c21902dafb3076ebdc6e12092f6434ebd6672d7f
Raman Nema <139612557+raman-nema@users.noreply.github.com>
2026-04-13 11:04:03 +0530

    Design Purchase Order Structure & DTOs.

--- HCL-LMS--master/backend/src/main/java/com/phegondev/InventoryManagementSystem/dto/CategoryDTO.java
+++ HCL-LMS--master/backend/src/main/java/com/phegondev/InventoryManagementSystem/dto/CategoryDTO.java
@@ -7,19 +7,42 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * DTO (Data Transfer Object) for Category
+ *
+ * This class is used to transfer category data between
+ * client (API request/response) and server.
+ *
+ * NOTE:
+ * DTO helps in hiding internal entity structure and ensures
+ * clean separation between API layer and database layer.
+ */
+@Data // Generates getters, setters, toString, equals, hashCode automatically
+@AllArgsConstructor // Generates constructor with all fields
+@NoArgsConstructor  // Generates default constructor
+@JsonInclude(JsonInclude.Include.NON_NULL) 
+// Only include non-null fields in JSON response
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonIgnoreProperties(ignoreUnknown = true) 
+// Ignores extra fields in incoming JSON request (prevents errors)
+
 public class CategoryDTO {
 
+    // Unique identifier of category
     private Long id;
 
+    /**
+     * Name of the category
+     * Validation:
+     * - Must not be blank (null or empty string not allowed)
+     */
     @NotBlank(message = "Name is required")
     private String name;
 
+    /**
+     * Number of products under this category
+     * This is generally used for response purpose
+     * (not required during creation)
+     */
     private Long productCount;
-
 }
